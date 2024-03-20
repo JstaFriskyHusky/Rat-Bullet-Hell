7e1f51bff3e06ede73c4a48f8bf1355431f8ff13
Ubermojo <163065777+Ubermojo@users.noreply.github.com>
2024-03-20 14:48:46 -0700

    Update Enemy.cs

--- Cheese Arsenal/Assets/Scripts/EnemyScripts/Enemy.cs
+++ Cheese Arsenal/Assets/Scripts/EnemyScripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     public int health = 1;
     public GameObject deathEffect;
 
+    // Allows this creature to take damage
     public void TakeDamage(int damage)
     {
         health -= damage;
@@ -19,8 +20,10 @@ public class Enemy : MonoBehaviour
 
     void Die()
     {
-        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        // Create death effect, then destroy this object and its effect
+        GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        Destroy(effect, 0.1f);
     }
 
 

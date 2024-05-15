d077b41e811991fe1184a58af13bf53b2d62de1c
Ubermojo <163065777+Ubermojo@users.noreply.github.com>
2024-05-15 09:44:49 -0700

    Zombie just drops a lil goo ball now

--- Cheese Arsenal/Assets/Scripts/EnemyScripts/Projectiles/EnemyBileProjectile.cs
+++ Cheese Arsenal/Assets/Scripts/EnemyScripts/Projectiles/EnemyBileProjectile.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyBileProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float maxLife = 2.0f;
+    private float lifeBtwTimer;
+
+
+
+    private void Update()
     {
-        
+        lifeBtwTimer += Time.deltaTime;
+        if(lifeBtwTimer >= maxLife)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
